f0b7727b73025aff29ea20a51d1c00c97f5d55ba
Raymond Cothern <rtcothern@gmail.com>
2015-01-18 22:52:50 -0800

    Added a fade transition between scenes

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneTransition : MonoBehaviour {
+
+	public int nextLevelNum = 1;
+
+	void OnTriggerEnter(Collider other) { 
+		//Application.LoadLevel(nextLevelNum); 
+		AutoFade.LoadLevel (nextLevelNum, 2.5f, 1.0f, Color.black);
+	}
+}
