930f32a249ec97f3aac72136a1905bdb6110cc7a
Raymond Cothern <rtcothern@gmail.com>
2015-01-21 13:24:23 -0800

    Initial Cinematic Camera Support
    Explored lerp functionality

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform[] views;
+	public float[] transitionDurations;
+
+	public Transform startMarker;
+	public Transform endMarker;
+	public float speed = 1.0F;
+	private float startTime;
+	private float journeyLength;
+	public float smooth = 5.0F;
+	void Start() {
+		startTime = Time.time;
+		startMarker = transform;
+		journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+	}
+	void Update() {
+		float distCovered = (Time.time - startTime) * speed;
+		float fracJourney = distCovered / journeyLength;
+		transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+	}
+}
