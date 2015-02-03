14b025f2c036149c3d99a78d9926831ff1eab37c
Raymond Cothern <rtcothern@gmail.com>
2015-02-03 07:00:04 -0800

    Started on lumberjack sc3

--- Assets/Scripts/LumberjackController.cs
+++ Assets/Scripts/LumberjackController.cs
@@ -5,33 +5,42 @@ public class LumberjackController : MonoBehaviour {
 
 	public int sceneNumber;
 	public GameObject animal;
-	
-	// Use this for initialization
+
+	private Animator animator;
+	private Vector3 lastPos;
+//	 Use this for initialization
 	void Start () {
 		switch (sceneNumber) {
-		case 0:
+		case 1:
 			print("Lumberjack: Chopping Wood");
 			break;
-	   	case 2:
+	   	case 3:
 			print ("Lumberjack: Searching for dog");
+			lastPos = transform.position;
+			animator = GetComponent<Animator>();
 			break;
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float distance = Vector3.Distance (animal.transform.position, transform.position);
 		switch (sceneNumber) {
-		case 0:
+		case 1:
+			float distance = Vector3.Distance (animal.transform.position, transform.position);
 			if(distance > 30f && distance < 40f)
 				print("Lumberjack: Calling out to dog to come back");
 			else if(distance >= 40f)
 				print("Lumberjack: Decides to let him go, look for him if not back by dark");
 			break;
-		case 2:
-			if (distance < 10)
-				print("Lumberjack: Kills wolf (after a truly epic battle) and is reunited with dog");
+		case 3:
+//			if (distance < 10)
+			Vector3 velocity = transform.position - lastPos;
+			lastPos = transform.position;
+			animator.SetFloat("Speed", velocity.magnitude);
+			print ("Speed: " + velocity.magnitude);
+			print("Lumberjack: Kills wolf (after a truly epic battle) and is reunited with dog");
 			break;
 		}
 	}
+	
 }
