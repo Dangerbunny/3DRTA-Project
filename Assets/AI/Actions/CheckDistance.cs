6bc57167ac7a0bd3941183d183ed8cf477fa1975
Raymond Cothern <rtcothern@gmail.com>
2015-02-27 21:44:50 -0800

    Added boy follow AI in RAIN, added some sounds to scene 1

---
+++ Assets/AI/Actions/CheckDistance.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Action;
+using RAIN.Core;
+using RAIN.Representation;
+using RAIN.Motion;
+
+[RAINDecision("CheckDistance")]
+public class CheckDistance : RAINDecision
+{
+	private int _lastRunning = 0;
+	
+	public Expression Target = new Expression();
+	public Expression GreaterThan = new Expression();
+	public Expression LessThan = new Expression();
+	
+	public override void Start(RAIN.Core.AI ai)
+	{
+		base.Start(ai);
+		
+		_lastRunning = 0;
+	}
+	
+	public override ActionResult Execute(RAIN.Core.AI ai)
+	{
+		ActionResult tResult = ActionResult.RUNNING;
+
+		Debug.Log ("Target:" + Target);
+
+		if (Target.IsValid && Target.IsVariable)
+		{
+			Debug.Log("MADE IT");
+			MoveLookTarget tTarget = MoveLookTarget.GetTargetFromVariable(ai.WorkingMemory, Target.VariableName);
+			float tDistance = (tTarget.Position - ai.Kinematic.Position).magnitude;
+			
+			if (GreaterThan.IsValid && (tDistance <= GreaterThan.Evaluate<float>(ai.DeltaTime, ai.WorkingMemory)))
+				return ActionResult.FAILURE;
+			
+			if (LessThan.IsValid && (tDistance >= LessThan.Evaluate<float>(ai.DeltaTime, ai.WorkingMemory)))
+				return ActionResult.FAILURE;
+		}
+		
+		
+		for (; _lastRunning < _children.Count; _lastRunning++)
+		{
+			tResult = _children[_lastRunning].Run(ai);
+			if (tResult != ActionResult.SUCCESS)
+				break;
+		}
+		
+		return tResult;
+	}
+}
--- Assets/_Scripts/BoyController.cs
+++ Assets/_Scripts/BoyController.cs
@@ -22,18 +22,18 @@ public class BoyController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (elder.gameObject.activeInHierarchy == false){
-			gameObject.SetActive (false);
-			return;
-		}
-		Vector3 elders_side = elder.position+elders_side_offset;
-		float distance = Vector3.Distance(elders_side, transform.position);
-		if (distance <= 2f) {
-			animator.SetBool ("NearElder", true);
-		} else {
-			animator.SetBool("NearElder", false);
-		}
-		
-		agent.SetDestination(elders_side);
+//		if (elder.gameObject.activeInHierarchy == false){
+//			gameObject.SetActive (false);
+//			return;
+//		}
+//		Vector3 elders_side = elder.position+elders_side_offset;
+//		float distance = Vector3.Distance(elders_side, transform.position);
+//		if (distance <= 2f) {
+//			animator.SetBool ("NearElder", true);
+//		} else {
+//			animator.SetBool("NearElder", false);
+//		}
+//		
+//		agent.SetDestination(elders_side);
 	}
 }
